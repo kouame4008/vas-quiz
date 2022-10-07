26cffc95533e172a00297798c23c2dadabb47fb9
Kouame Jean <jeanmarcokassa@gmail.com>
2022-10-07 15:51:02 +0000

    yggrfda

--- shared/layouts/LayoutBlanc.tsx
+++ shared/layouts/LayoutBlanc.tsx
@@ -6,6 +6,7 @@ import LogoQuiz from '../../public/assets/Header-logo-blue.png';
 import styled from 'styled-components';
 import FooterBottom from '../components/footer/FooterBottom';
 
+const Fade = require('react-reveal/Fade')
 
 
 const Bottom = styled.div`
@@ -20,7 +21,13 @@ const LayoutBlanc = ({ children }: { children: React.ReactNode }) => {
             <Navbar style={{ boxShadow: 'rgba(0, 0, 0, 0.01) 0px 10px 27px' }}>
                 <Header logo={LogoQuiz} />
             </Navbar>
-            {children}
+
+            <section style={{ marginTop: '1rem' }}>
+                <Fade top>
+                    {children}
+                </Fade>
+            </section>
+
             <Bottom>
                 <FooterBottom />
             </Bottom>
